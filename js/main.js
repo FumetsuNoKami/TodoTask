f1d52e209dedb7cece03b6166cecced76e382747
Andrew <Andrey_U_O@mail.ru>
2023-11-29 19:43:16 +0300

    finaly fixed all

--- js/main.js
+++ js/main.js
@@ -8,72 +8,97 @@ parity = length % 2 === 0;
 const completedBtn = document.getElementsByClassName("completedBtn");
 const deleteBtn = document.getElementsByClassName("deleteBtn");
 const storageValues = [];
-for (let i = 0; i < localStorage.length; i++) {
-  storageValues.push(localStorage.getItem(localStorage.key(i)));
-}
-const createListItem = () => {
-  try {
-    const addItem = document.createElement("li");
-    const divadd = document.createElement("div");
-    const completing = document.createElement("button");
-    const deleting = document.createElement("button");
-
-    completing.classList.add("completedBtn");
-    completing.setAttribute("style", "margin-right: 0;");
-    completing.innerHTML = "✔";
-    completing.addEventListener("click", () => {
-      if (completing.closest("li").classList.contains("completedItem")) {
-        completing.closest("li").classList.remove("completedItem");
-      } else completing.closest("li").classList.add("completedItem");
+if (JSON.parse(localStorage.getItem("tasks")) != null)
+  for (let i = 0; i < JSON.parse(localStorage.getItem("tasks")).length; i++) {
+    storageValues.push({
+      text: JSON.parse(localStorage.getItem("tasks"))[i].text,
+      status: JSON.parse(localStorage.getItem("tasks"))[i].status,
     });
-
-    deleting.classList.add("deleteBtn");
-    deleting.innerHTML = "✖";
-    deleting.addEventListener("click", () => {
-      deleting.closest("li").remove();
-      const arrayRemoveItem = deleting
-        .closest("li")
-        .textContent.substring(
-          0,
-          deleting.closest("li").textContent.length - 2,
-        );
-      const arrayIndexOfRemoveItem = storageValues.indexOf(arrayRemoveItem, 0);
-      storageValues.splice(arrayIndexOfRemoveItem, 1);
-      localStorage.removeItem(arrayRemoveItem);
-      i--;
-    });
-    divadd.append(completing);
-    divadd.append(deleting);
-
-    if (i < localStorage.length) {
-      addItem.innerHTML = storageValues[i];
-    } else if (inputField.value.trim() === "") {
-      return;
-    }
-    if (i >= localStorage.length) {
-      storageValues.push(inputField.value);
-      addItem.innerHTML = inputField.value;
-      localStorage.setItem(inputField.value, inputField.value);
-    }
-    if (parity === false) {
-      addItem.setAttribute("class", "listItem even");
-      addItem.append(divadd);
-      list.append(addItem);
-      parity = !parity;
-    } else {
-      addItem.setAttribute("class", "listItem odd");
-      addItem.append(divadd);
-      list.append(addItem);
-      parity = !parity;
-    }
-    i++;
-    inputField.value = "";
-  } catch (error) {
-    console.log(error);
-    //localStorage.clear();
   }
+const createListItem = () => {
+  const addItem = document.createElement("li");
+  const divadd = document.createElement("div");
+  const completing = document.createElement("button");
+  const deleting = document.createElement("button");
+
+  completing.classList.add("completedBtn");
+  completing.setAttribute("style", "margin-right: 0;");
+  completing.innerHTML = "✔";
+  completing.addEventListener("click", () => {
+    const liText = completing
+      .closest("li")
+      .textContent.substring(
+        0,
+        completing.closest("li").textContent.length - 2,
+      );
+    let buf = [];
+    for (let j = 0; j < storageValues.length; j++) {
+      buf.push(storageValues[j].text);
+    }
+
+    const completeIndex = buf.indexOf(liText, 0);
+
+    if (completing.closest("li").classList.contains("completedItem")) {
+      storageValues[completeIndex].status = "Not completed";
+      completing.closest("li").classList.remove("completedItem");
+    } else {
+      storageValues[completeIndex].status = "completed";
+      completing.closest("li").classList.add("completedItem");
+    }
+    localStorage.setItem("tasks", JSON.stringify(storageValues));
+  });
+  deleting.classList.add("deleteBtn");
+  deleting.innerHTML = "✖";
+  deleting.addEventListener("click", () => {
+    deleting.closest("li").remove();
+    let buf = [];
+    for (let j = 0; j < storageValues.length; j++) {
+      buf.push(storageValues[j].text);
+    }
+    const liText = deleting
+      .closest("li")
+      .textContent.substring(0, deleting.closest("li").textContent.length - 2);
+    const completeIndex = buf.indexOf(liText, 0);
+    storageValues.splice(completeIndex, 1);
+    localStorage.setItem("tasks", JSON.stringify(storageValues));
+    i--;
+  });
+  divadd.classList.add("divadd");
+  divadd.append(completing);
+  divadd.append(deleting);
+
+  if (i < JSON.parse(localStorage.getItem("tasks")).length) {
+    addItem.innerHTML = storageValues[i].text;
+  } else if (inputField.value.trim() === "") {
+    return;
+  }
+  if (i >= JSON.parse(localStorage.getItem("tasks")).length) {
+    storageValues.push({ text: inputField.value, status: "Not completed" });
+    addItem.innerHTML = inputField.value;
+  }
+  if (parity === false) {
+    addItem.setAttribute("class", "listItem even");
+    addItem.append(divadd);
+    list.append(addItem);
+    parity = !parity;
+  } else {
+    addItem.setAttribute("class", "listItem odd");
+    addItem.append(divadd);
+    list.append(addItem);
+    parity = !parity;
+  }
+  if (storageValues[i].status === "completed")
+    completing.closest("li").classList.add("completedItem");
+  i++;
+  inputField.value = "";
+  localStorage.setItem("tasks", JSON.stringify(storageValues));
 };
-for (let i = 0; i < localStorage.length; i++) {
-  createListItem();
+
+if (JSON.parse(localStorage.getItem("tasks")) != null)
+  for (let i = 0; i < JSON.parse(localStorage.getItem("tasks")).length; i++) {
+    createListItem();
+  }
+if (JSON.parse(localStorage.getItem("tasks")) == null) {
+  localStorage.setItem("tasks", "[]");
 }
 addBtn.addEventListener("click", createListItem);
